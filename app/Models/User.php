c8b8b66548ba6673f41d5e0725e009547a6ecee7
Muhamed Mehmedovic <54117777+Muhamed112@users.noreply.github.com>
2021-07-29 18:39:53 +0200

    Generate Question model via artisan model generator

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -31,6 +32,15 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function questions() {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value,'-');
+    }
 
     /**
      * The attributes that should be cast to native types.
